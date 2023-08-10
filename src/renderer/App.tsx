b29291564d0ce3b59ad98b90c7430754a1fc8250
kylemaestro <kyalmeister@gmail.com>
2023-08-09 20:04:08 -0500

    Add project card list

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,6 +1,6 @@
 import './App.css';
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
-import { Avatar, Badge, Box, Button, ChakraProvider, Flex, ThemeConfig, Text, extendTheme, useColorMode } from '@chakra-ui/react';
+import { Avatar, Badge, Box, Button, ChakraProvider, Flex, ThemeConfig, Text, extendTheme, useColorMode, Center, ButtonGroup, Card, CardBody, CardFooter, Divider, Heading, Image, Stack } from '@chakra-ui/react';
 import icon from '../../assets/duck.svg';
 import { useEffect, useState } from 'react';
 
@@ -131,9 +131,162 @@ function SplashScreen() {
                 : "fadeIn .3s .4s forwards"
             }
           >
-            <Badge borderRadius='2px' px='3' colorScheme='red'>
-              Error!
-            </Badge>
+            <Center p={4}>
+              <Box overflowY="auto" maxHeight="calc(100vh - 325px)" p={4}> {/* Adjust maxHeight as required */}
+                <Stack spacing={4}>
+                  <Card
+                    direction={{ base: 'column', sm: 'row' }}
+                    overflow='hidden'
+                    variant='outline'
+                    h={120}
+                  >
+                    <Image
+                      objectFit='cover'
+                      maxW={{ base: '100%', sm: '200px' }}
+                      src='https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60'
+                      alt='Caffe Latte'
+                    />
+
+                    <Stack spacing={4}>
+                      <CardBody>
+                        <Heading size='md'>The perfect latte</Heading>
+
+                        <Text py='2'>
+                          Caffè latte is a coffee beverage of Italian origin made with espresso
+                          and steamed milk.
+                        </Text>
+                      </CardBody>
+
+                      <CardFooter>
+                        <Button variant='solid' colorScheme='blue'>
+                          Buy Latte
+                        </Button>
+                      </CardFooter>
+                    </Stack>
+                  </Card>
+                  <Card
+                    direction={{ base: 'column', sm: 'row' }}
+                    overflow='hidden'
+                    variant='outline'
+                    h={120}
+                  >
+                    <Image
+                      objectFit='cover'
+                      maxW={{ base: '100%', sm: '200px' }}
+                      src='https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60'
+                      alt='Caffe Latte'
+                    />
+
+                    <Stack spacing={4}>
+                      <CardBody>
+                        <Heading size='md'>The perfect latte</Heading>
+
+                        <Text py='2'>
+                          Caffè latte is a coffee beverage of Italian origin made with espresso
+                          and steamed milk.
+                        </Text>
+                      </CardBody>
+
+                      <CardFooter>
+                        <Button variant='solid' colorScheme='blue'>
+                          Buy Latte
+                        </Button>
+                      </CardFooter>
+                    </Stack>
+                  </Card>
+                  <Card
+                    direction={{ base: 'column', sm: 'row' }}
+                    overflow='hidden'
+                    variant='outline'
+                    h={120}
+                  >
+                    <Image
+                      objectFit='cover'
+                      maxW={{ base: '100%', sm: '200px' }}
+                      src='https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60'
+                      alt='Caffe Latte'
+                    />
+
+                    <Stack spacing={4}>
+                      <CardBody>
+                        <Heading size='md'>The perfect latte</Heading>
+
+                        <Text py='2'>
+                          Caffè latte is a coffee beverage of Italian origin made with espresso
+                          and steamed milk.
+                        </Text>
+                      </CardBody>
+
+                      <CardFooter>
+                        <Button variant='solid' colorScheme='blue'>
+                          Buy Latte
+                        </Button>
+                      </CardFooter>
+                    </Stack>
+                  </Card>
+                  <Card
+                    direction={{ base: 'column', sm: 'row' }}
+                    overflow='hidden'
+                    variant='outline'
+                    h={120}
+                  >
+                    <Image
+                      objectFit='cover'
+                      maxW={{ base: '100%', sm: '200px' }}
+                      src='https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60'
+                      alt='Caffe Latte'
+                    />
+
+                    <Stack spacing={4}>
+                      <CardBody>
+                        <Heading size='md'>The perfect latte</Heading>
+
+                        <Text py='2'>
+                          Caffè latte is a coffee beverage of Italian origin made with espresso
+                          and steamed milk.
+                        </Text>
+                      </CardBody>
+
+                      <CardFooter>
+                        <Button variant='solid' colorScheme='blue'>
+                          Buy Latte
+                        </Button>
+                      </CardFooter>
+                    </Stack>
+                  </Card>
+                  <Card
+                    direction={{ base: 'column', sm: 'row' }}
+                    overflow='hidden'
+                    variant='outline'
+                    h={120}
+                  >
+                    <Image
+                      objectFit='cover'
+                      maxW={{ base: '100%', sm: '200px' }}
+                      src='https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60'
+                      alt='Caffe Latte'
+                    />
+
+                    <Stack spacing={4}>
+                      <CardBody>
+                        <Heading size='md'>The perfect latte</Heading>
+
+                        <Text py='2'>
+                          Caffè latte is a coffee beverage of Italian origin made with espresso
+                          and steamed milk.
+                        </Text>
+                      </CardBody>
+
+                      <CardFooter>
+                        <Button variant='solid' colorScheme='blue'>
+                          Buy Latte
+                        </Button>
+                      </CardFooter>
+                    </Stack>
+                  </Card>
+                </Stack>
+              </Box>
+            </Center>
           </Box>
           )}
         </div>

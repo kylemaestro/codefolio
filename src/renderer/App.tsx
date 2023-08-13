ed2971f88957b2805f2960659f74fa4ab0ac4b53
kylemaestro <kyalmeister@gmail.com>
2023-08-13 13:09:25 -0500

    Add dotenv config

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,3 +1,4 @@
+require('dotenv').config();
 import './App.css';
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import { Avatar, Badge, Box, Button, ChakraProvider, Flex, Icon, InputRightAddon,
